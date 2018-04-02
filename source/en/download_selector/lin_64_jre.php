a41cf591bd38a4fcb7cff340517857f134974798
xulihang <xulihanghai@163.com>
2018-04-02 15:41:45 +0800

    add project files

---
+++ source/en/download_selector/lin_64_jre.php
@@ -0,0 +1,71 @@
+<?php print "<?xml version=\"1.0\"?>" ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  
+  <title>OmegaT, the free translation memory tool</title>
+  <meta name="generator" content="screem 0.10.2" />
+  
+  <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+  
+  <meta name="keywords" content="translation memory, TM, computer-aided translation, computer-assisted translation, CAT, GPL, open source" />
+
+  
+<link rel="stylesheet" type="text/css" href="../../config/ds.css" />
+</head>
+
+<?php include '../../config/versions.txt'; ?>
+
+<body>
+
+<div id="container">
+			
+<div id="banner">
+
+<h1>OmegaT</h1>
+
+<p>The free (GPL) translation memory tool</p>
+
+</div>
+
+<div id="content">
+		
+<h2>OmegaT Download Selector</h2>
+
+<h3>Step 4: "Standard" or "Latest"</h3>
+
+
+<table>
+
+<td><?php $link='<a
+href="http://sourceforge.net/projects/omegat/files/OmegaT%20-%20Latest/OmegaT%20'.$latest_name.'/OmegaT_'.$latest_number.'_Beta_Linux_64.tar.bz2/download"
+>';
+$endlink='</a>';?>
+<?php echo $link ?>Latest<?php echo $endlink ?></p></td>
+
+<td><p>This is the link to the "Latest" version of OmegaT for Linux with 64-bit JRE included. A "Standard" version of OmegaT for 64-bit Linux is not available.</p></td>
+
+</tr>
+
+<tr>
+
+<td><a href="../downloads.html">Quit</a></td>
+
+<td><p>Quit the Download Selector and return to the main downloads page.</p></td>
+
+</tr>
+
+</table>
+
+</div>
+
+<div id="footer"><a href="http://sourceforge.net"><img src="http://sourceforge.net/sflogo.php?group_id=68187&amp;type=1" alt="SourceForge.net Logo" height="31" width="88" /></a><br />
+
+<a href="http://validator.w3.org/check?uri=referer"><img src="../../images/xhtml.png" alt="Valid XHTML" /></a> <a href="http://jigsaw.w3.org/css-validator/check/referer"><img src="../../images/css.png" alt="Valid XHTML" /></a>
+	</div>
+
+</div>
+
+</body>
+
+</html>
